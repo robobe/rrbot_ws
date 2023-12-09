701715d06082a3dc030700648b7cefc4cccb1574
robo <robo2020@gmail.com>
2023-12-09 15:55:23 +0200

    set joint2 to effort control

---
+++ src/rrbot_bringup/launch/control.launch.py
@@ -0,0 +1,31 @@
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+PKG = "rrbot_bringup"
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    position_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["position_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    effort_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["effort_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    ld.add_action(joint_state_broadcaster_spawner)
+    ld.add_action(position_controller_spawner)
+    ld.add_action(effort_controller_spawner)
+    return ld
