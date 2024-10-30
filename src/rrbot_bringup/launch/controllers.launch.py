f9d91af602caa21ecbe2c68af1cd7255e919942b
robo2020 <robo2020@gmail.com>
2024-10-30 20:52:00 +0000

    change launch

--- src/rrbot_bringup/launch/controllers.launch.py
+++ src/rrbot_bringup/launch/controllers.launch.py
@@ -3,7 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 import xacro
 from pathlib import Path
-
+from launch.substitutions import Command
 
 URDF = "rrbot.urdf.xacro"
 PKG_DESCRIPTION = "rrbot_description"
@@ -18,16 +18,24 @@ def generate_launch_description():
 
     robot_description = {"robot_description": urdf}
     
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
     pkg_bringup = get_package_share_directory(PKG_BRINGUP)
     robot_controllers = Path(pkg_bringup).joinpath("config", "rrbot_position.yaml").as_posix()
-
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
+        parameters=[robot_controllers],
         output="both",
+        remappings=[('/controller_manager/robot_description', '/robot_description', )]
     )
 
+    ld.add_action(robot_state_publisher)
     ld.add_action(control_node)
 
     return ld
